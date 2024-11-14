c2064e796a76754f0839ae9401c736f3e510b236
sknacook03 <pongwit27@gmail.com>
2024-11-14 18:54:47 +0700

    fix app homepage

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
 import {
   createBrowserRouter,
   RouterProvider,
@@ -7,11 +8,14 @@ import {
   Link,
 } from "react-router-dom";
 
-import App from './App.jsx'
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />
+  },
+  {
+    path: "/",
+    element: <Register />
   },
 ]);
 
