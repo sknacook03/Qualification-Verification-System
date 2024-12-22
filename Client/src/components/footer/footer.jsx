2c43e964c23e3b21d9bccfb6d2e27f08f9eee279
sknacook03 <pongwit27@gmail.com>
2024-12-22 19:01:04 +0700

    update Homepage

--- Client/src/components/footer/footer.jsx
+++ Client/src/components/footer/footer.jsx
@@ -1,20 +1,29 @@
 import "./footer.css";
 import { Link } from "react-router-dom";
-function Footer({ color }) {
+
+function Footer({ color, disableMenu }) {
   return (
     <>
       <footer className="contrainer-footer">
         <div className="copy">
-        <h5 style={{ color: color }}>
+          <h5 style={{ color: color }}>
             Developed by Khon Rak Khrop Khrua and Khon Chao Choo © Rajamangala
-            University of Technology Isan 2024 -  ระบบตรวจสอบคุณวุฒิ
+            University of Technology Isan 2024 - ระบบตรวจสอบคุณวุฒิ
           </h5>
         </div>
-        <div className="option">
-          <Link to="/LoginOfficer">สำหรับเจ้าหน้าที่</Link>
-          <Link to="/terms-of-Use">ข้อตกลงการใช้ระบบ</Link>
-          <Link to="/contact">ติดต่อ</Link>
-        </div>
+        {!disableMenu && (
+          <div className="option">
+            <Link to="/LoginOfficer" style={{ color: color }}>
+              สำหรับเจ้าหน้าที่
+            </Link>
+            <Link to="/terms-of-Use" style={{ color: color }}>
+              ข้อตกลงการใช้ระบบ
+            </Link>
+            <Link to="/contact" style={{ color: color }}>
+              ติดต่อ
+            </Link>
+          </div>
+        )}
       </footer>
     </>
   );
