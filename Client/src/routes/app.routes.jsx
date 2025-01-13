c6b357f605b8aa8330f0b80414b85a9db6b5551f
sknacook03 <pongwit27@gmail.com>
2025-01-13 18:26:36 +0700

    add new page agency

--- Client/src/routes/app.routes.jsx
+++ Client/src/routes/app.routes.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import Homepages from "../pages/Homepages/Homepages.jsx";
+import CheckQualificationsPage from "../pages/CheckQualificationsPage/CheckQualificationsPage.jsx";
+import AccessStatisticsPage from "../pages/AccessStatisticsPage/AccessStatisticsPage.jsx";
+import PrivacySettingsPage from "../pages/PrivacySettingsPage/PrivacySettingsPage.jsx";
 import Contact from "../layout/contact/contact.jsx";
 
 const appRoutes = [
   { path: "/Homepages", element: <Homepages /> },
+  { path: "/CheckQualificationsPage", element: <CheckQualificationsPage /> },
+  { path: "/AccessStatisticsPage", element: <AccessStatisticsPage /> },
+  { path: "/PrivacySettingsPage", element: <PrivacySettingsPage /> },
   { path: "/contact", element: <Contact /> },
 ];
 
