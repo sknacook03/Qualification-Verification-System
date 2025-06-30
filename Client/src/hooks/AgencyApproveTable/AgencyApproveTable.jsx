d256cb46d7f4d233a5471281557278980bab07b5
Jayyz <jdai254725@gmail.com>
2025-06-30 22:05:40 +0700

    Reflactor AgencyAproveTable a littel bit.

--- Client/src/hooks/AgencyApproveTable/AgencyApproveTable.jsx
+++ Client/src/hooks/AgencyApproveTable/AgencyApproveTable.jsx
@@ -120,7 +120,7 @@ function AgencyApproveTable({
                 </td>
                 <td data-label="ประเภทหน่วยงาน">
                   {typeAgencies.find((t) => t.id === agencyItem.type_id)
-                    ?.type_name || agencyItem.type_id}
+                    ?.type_name || "Loading..."}
                 </td>
                 <td data-label="หนังสือรับรอง">
                   <div className={styles.imageContainer}>
