d03e64afc26024ebbe5b3b6260355cceddf74288
sknacook03 <pongwit27@gmail.com>
2025-09-07 23:05:20 +0700

    fix: set default port to 3001 if not specified in environment variables

--- Server/server.js
+++ Server/server.js
@@ -1,7 +1,7 @@
 import app from "./app.js";
 import dotenv from "dotenv"
 dotenv.config();
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3001
 
 app.listen(PORT, () => {
   console.log(`Server on port ${PORT}`);
