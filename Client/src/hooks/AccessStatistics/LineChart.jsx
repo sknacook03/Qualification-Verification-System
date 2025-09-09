f801298fa5e509edf7e928972c0b142197e58b8e
Jayyz <jdai254725@gmail.com>
2025-09-09 21:02:06 +0700

    refactor: clean up LineChart component by removing unnecessary whitespace

--- Client/src/hooks/AccessStatistics/LineChart.jsx
+++ Client/src/hooks/AccessStatistics/LineChart.jsx
@@ -2,8 +2,7 @@ import React from "react";
 import { Chart } from "react-chartjs-2";
 
 const LineChart = ({ data, options }) => {
-
-
+  
   return <Chart type="line" data={data} options={options} />;
 };
 
--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import routes from './routes/routes.jsx'
+import 'chart.js/auto'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
