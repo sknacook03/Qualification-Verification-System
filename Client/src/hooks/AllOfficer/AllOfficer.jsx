892d0ac941222bafbbcf3e28eae51f71ff28cbb5
sknacook03 <pongwit27@gmail.com>
2026-01-22 11:40:55 +0700

    fix: update index display for officers to reflect correct pagination offset

--- Client/src/hooks/AllOfficer/AllOfficer.jsx
+++ Client/src/hooks/AllOfficer/AllOfficer.jsx
@@ -158,7 +158,7 @@ function AllOfficer() {
             <tbody>
               {currentItems.map((officer, index) => (
                 <tr key={officer.id}>
-                  <td data-label="ลำดับ"> {index + 1} </td>
+                  <td data-label="ลำดับ"> {offset + index + 1} </td>
                   <td data-label="ชื่อ"> {officer.first_name} </td>
                   <td data-label="นามสกุล"> {officer.last_name} </td>
                   <td data-label="อีเมล"> {officer.email} </td>
